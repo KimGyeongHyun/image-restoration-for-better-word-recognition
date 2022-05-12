14d9833d3f0a265288ce38f1c25e3c78b98e7072
KimGyeongHyun <105264179+KimGyeongHyun@users.noreply.github.com>
2022-05-12 19:51:37 +0900

    week11 hw9 answer

--- week11 hw9.py
+++ week11 hw9.py
@@ -1,6 +1,8 @@
-
 import numpy as np
+import cv2
 import math
+import matplotlib.pyplot as plt
+
 
 def zigzag(input):
     h, v, i = 0,0,0
@@ -45,6 +47,7 @@ def zigzag(input):
             output[i] = input[v, h]
             break
     return output
+
 
 def inverse_zigzag(input):
     h, v, i = 0,0,0
@@ -91,6 +94,9 @@ def inverse_zigzag(input):
             break
     return output
 
+
+img = cv2.imread('lena.jpg', cv2.IMREAD_GRAYSCALE)
+
 N = 8
 C = []
 for i in range(N):
@@ -107,17 +113,32 @@ for i in range(N):
 
 C = np.array(C)
 CT = np.transpose(C)
-print(C)
 
+H, W = img.shape
+X = np.zeros((int(H/8), int(W/8), 8, 8))    # 이미지를 8*8로 저장할 배열
+# X_hat = np.zeros((H, W))
 
-X = np.random.randint(0,255,size=(8, 8))
-print(X)
+# 이미지를 8*8로 쪼개서 저장
+for i in range(int(H/8)):
+    for j in range(int(W/8)):
+        X[i, j,:, :] = img[i * 8 : i * 8 + 8, j * 8 : j * 8 + 8]
 
-Y1 = np.matmul(C, X)    # CX
-Y = np.round(np.matmul(Y1, CT)) # CX * CT
-print(Y)
+print("image 0 1 big")
+print(X[0, 0])
+print(X[0, 1])
+print(X[50, 50])
 
+# Y 에 DCT 저장
+Y1 = np.zeros((int(H/8), int(W/8), 8, 8))
+Y = np.zeros((int(H/8), int(W/8), 8, 8))
+for i in range(int(H/8)):
+    for j in range(int(W/8)):
+        Y1[i, j, :, :] = np.matmul(C, X[i, j])
+        Y[i, j, :, :] = np.round(np.matmul(Y1[i, j], CT))
 
+print("DCT 0, big")
+print(Y[0, 0])
+print(Y[50, 50])
 
 Q = [[16,11,10,16,24,40,51,61],
      [12,12,14,19,26,58,60,55],
@@ -128,54 +149,143 @@ Q = [[16,11,10,16,24,40,51,61],
      [49,64,78,87,103,121,120,101],
      [72,92,95,98,112,100,103,99]]
 
-Y_hat = np.round(Y / Q)
-print(Y)
+# Y Quantization -> Y_hat
+Y_hat = np.zeros((int(H/8), int(W/8), 8, 8))
 
-zig_Y = zigzag(Y_hat)
-print(zig_Y)
+for i in range(int(H/8)):
+    for j in range(int(W/8)):
+        Y_hat[i, j,:, :] = np.round(Y[i, j] / Q)
 
+print("Q 0, big")
+print(Y_hat[0, 0])
+print(Y_hat[50, 50])
 
+# Y_hat zigzag scan
+zig_Y = np.zeros((int(H/8), int(W/8), 64))
+for i in range(int(H/8)):
+    for j in range(int(W/8)):
+        zig_Y[i, j] = zigzag(Y_hat[i, j])
+
+print("zigzag 0, 1, big")
+print(zig_Y[0, 0])
+print(zig_Y[0, 1])
+print(zig_Y[50, 50])
+
+# Run length coding
 run_level = list()
 num_zero = 0
+alert = 1
 
-for i in zig_Y:
-    if len(run_level) == 0:
-        run_level.append(i)
-    elif i != 0:
-        run_level.append((num_zero, i))
+for i in range(int(H/8)):
+    for j in range(int(W/8)):
+        for k in zig_Y[i, j]:
+            if alert == 1:
+                alert = 0
+                run_level.append(k)
+            elif k != 0:
+                run_level.append((num_zero, k))
+                num_zero = 0
+            else:
+                num_zero += 1
+        run_level.append("EOB")
         num_zero = 0
-    else:
-        num_zero += 1
+        alert = 1
 
-run_level.append("EOB")
-print(run_level)
+print("run level ~15")
+print(run_level[0:15])
 
-de_run_level = np.zeros(64)
+# De run length coding
+de_run_level = np.zeros((int(H/8), int(W/8), 64))
+
+column = 0
+row = 0
 index = 0
+alert = 1
+# i[0] : 첫번째 0의 갯수, i[1] : 0 이후에 오는 숫자
 for i in run_level:
-    if not np.any(de_run_level):
-        de_run_level[index] = i
-        index += 1
+    """
+    print(i)
+    print(column)
+    print(row)
+    print()
+    """
+    if alert == 1:   # 처음일 때
+        de_run_level[column, row, index] = i    # DC값 설정
+        index += 1  # 다음으로 넘어감
+        alert = 0
 
-    elif i == 'EOB':
-        break
-    elif i[0] == 0:
-        de_run_level[index] = i[1]
-        index += 1
-    else:
-        num_zero = i[0]
-        while(num_zero > 0):
-            de_run_level[index] = 0
-            num_zero -= 1
-            index += 1
-        de_run_level[index] = i[1]
-        index += 1
+    elif i == 'EOB':    # EOB 일 때
+        index = 0   # 인덱스 초기화
+        alert = 1
+        if column == 63 and row == 63:    # 마지막이라면 종료
+            break
+        elif row == 63:  # 마지막 행일 때
+            column += 1 # 열을 옮김
+            row = 0     # 행 초기화
+        else:   # 마지막 행이 아닐 때
+            row += 1    # 행 옮김
 
-print(de_run_level)
+    elif i[0] == 0:  # 뒤에 0이 없는 경우
+        de_run_level[column, row, index] = i[1]  # 숫자를 설정
+        index += 1  # 다음으로 넘어감
 
-inv_zigzag = inverse_zigzag(de_run_level)
-print(inv_zigzag)
+    else:  # 뒤에 0이 있는 경우
+        num_zero = i[0]  # 0의 갯수를 num_zero에 저장
+        while (num_zero > 0):  # num_zero만큼 반복
+            de_run_level[column, row, index] = 0  # 0으로 설정
+            num_zero -= 1  # 0의 갯수를 한개 줄임
+            index += 1  # 다음으로 넘어감
+        de_run_level[column, row, index] = i[1]  # 0 이후에 나오는 숫자를 설정
+        index += 1  # 다음으로 넘어감
 
+print("de_run_levels 0, 1, 2, big")
+print(de_run_level[0, 0, :])
+print(de_run_level[0, 1, :])
+print(de_run_level[0, 2, :])
+print(de_run_level[50, 50, :])
 
-# 겹치는 부분 없도록 구현
-# 코드 안에서 달라지는 이유 찾기
+# inverse zigzag
+inv_zigzag = np.zeros((int(H/8), int(W/8), 8, 8))
+for i in range(int(H/8)):
+    for j in range(int(W/8)):
+        inv_zigzag[i, j, :, :] = inverse_zigzag(de_run_level[i, j])
+
+print("inv zigzag ~2 big")
+print(inv_zigzag[0, 0])
+print(inv_zigzag[50, 50])
+
+# Dequantization
+deq = np.zeros((int(H/8), int(W/8), 8, 8))
+for i in range(int(H/8)):
+    for j in range(int(W/8)):
+        deq[i, j, :, :] = inv_zigzag[i, j, :, :] * Q
+
+print("Dequantization 0, 1, big")
+print(deq[0, 0])
+print(deq[0, 1])
+print(deq[50, 50])
+
+# idct
+Cinv = np.linalg.inv(C)
+CTinv = np.linalg.inv(CT)
+
+CinvV_hat = np.zeros((int(H/8), int(W/8), 8, 8))
+X_hat = np.zeros((int(H/8), int(W/8), 8, 8))
+for i in range(int(H/8)):
+    for j in range(int(W/8)):
+        CinvV_hat[i, j, :, :] = np.matmul(Cinv, deq[i, j])
+        X_hat[i, j, :, :] = np.round(np.matmul(CinvV_hat[i, j], CTinv))
+
+print("X_hat 0 1, big")
+print(X_hat[0,0])
+print(X_hat[0,1])
+print(X_hat[50, 50])
+
+# sum to image
+img2 = np.zeros((H, W))
+for i in range(int(H/8)):
+    for j in range(int(W/8)):
+        img2[i * 8 : i * 8 + 8, j * 8 : j * 8 + 8] = X_hat[i, j, :, :]
+
+plt.imshow(img2, cmap='gray')
+plt.show()
--- week11.py
+++ week11.py
@@ -2,6 +2,7 @@
 import numpy as np
 import math
 
+
 def zigzag(input):
     h, v, i = 0,0,0
     output = np.zeros((64))
@@ -45,6 +46,7 @@ def zigzag(input):
             output[i] = input[v, h]
             break
     return output
+
 
 def inverse_zigzag(input):
     h, v, i = 0,0,0
@@ -91,6 +93,7 @@ def inverse_zigzag(input):
             break
     return output
 
+
 N = 8
 C = []
 for i in range(N):
@@ -135,7 +138,7 @@ zig_Y = zigzag(Y_hat)
 print(zig_Y)
 
 
-run_level = list()
+run_level = []
 num_zero = 0
 
 for i in zig_Y:
@@ -152,30 +155,54 @@ print(run_level)
 
 de_run_level = np.zeros(64)
 index = 0
+# i[0] : 첫번째 0의 갯수, i[1] : 0 이후에 오는 숫자
 for i in run_level:
-    if not np.any(de_run_level):
-        de_run_level[index] = i
-        index += 1
+    if not np.any(de_run_level):    # 처음 DC 값일 때
+        de_run_level[index] = i     # DC값 설정
+        index += 1  # 다음으로 넘어감
 
+    # 리스트 끝일 때
     elif i == 'EOB':
         break
-    elif i[0] == 0:
-        de_run_level[index] = i[1]
-        index += 1
-    else:
-        num_zero = i[0]
-        while(num_zero > 0):
-            de_run_level[index] = 0
-            num_zero -= 1
-            index += 1
-        de_run_level[index] = i[1]
-        index += 1
+
+    elif i[0] == 0: # 뒤에 0이 없는 경우
+        de_run_level[index] = i[1]  # 숫자를 설정
+        index += 1  # 다음으로 넘어감
+
+    else:    # 뒤에 0이 있는 경우
+        num_zero = i[0] # 0의 갯수를 num_zero에 저장
+        while(num_zero > 0):    # num_zero만큼 반복
+            de_run_level[index] = 0 # 0으로 설정
+            num_zero -= 1   # 0의 갯수를 한개 줄임
+            index += 1  # 다음으로 넘어감
+        de_run_level[index] = i[1]  # 0 이후에 나오는 숫자를 설정
+        index += 1  # 다음으로 넘어감
 
 print(de_run_level)
 
 inv_zigzag = inverse_zigzag(de_run_level)
 print(inv_zigzag)
 
+deq = inv_zigzag * Q
+print(deq)
+
+Cinv = np.linalg.inv(C)
+CTinv = np.linalg.inv(CT)
+
+CinvVhat = np.matmul(Cinv, deq)
+Xhat = np.round(np.matmul(CinvVhat, CTinv))
+
+print(Xhat)
 
 # 겹치는 부분 없도록 구현
 # 코드 안에서 달라지는 이유 찾기
+"""
+ inv DCT
+ V = CXCT
+ X 값을 기준으로 C가 구성
+ inverse할 시 X 를 구해야 하는데 X 에서 C를 구할 수 없음
+ 1) C를 저장
+ 2) 또다른 inverse 과정이 있음
+ 과제 내에서는 역행렬을 구하라고 나와있음
+ 1 번의 과정
+"""
