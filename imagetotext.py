86a2aaaad914b08700f4b723b3db82cfe086f0bc
KimGyeongHyun <105264179+KimGyeongHyun@users.noreply.github.com>
2022-05-19 15:59:54 +0900

    define, flag 추가 / 커널사이즈 고정 / 미디안필터 추가

--- imagetotext.py
+++ imagetotext.py
@@ -5,11 +5,27 @@ import os
 # import matplotlib.pyplot as plt
 
 # define
-BINARY_STANDARD = 170   # binary image 만들때 화소값 기준
+BINARY_STANDARD = 170   # binary image 만들때 화소값 기준   이미지마다 다른 값 대입이 좋아보임     Eq 잘 되면 조절 필요 없을듯
+NONE = 0
+EROSION = 1
+DILATION = 2
+OPENING = 3
+CLOSING = 4
+
+# 필터를 수행할지 결정하는 플래그
+# 아래 순서로 필터링 진행
+FIRST_LINEAR_HQ = False
+ADAPTIVE = False
+MEDIAN = False
+flag_homomorphic = True
+HOMOMORPHIC = flag_homomorphic
+SECOND_LINEAR_HQ = flag_homomorphic
+BINARY = False
+MORPHOLOGY = False
 
 
 # 최종 이미지 필터 /   필터링된 이미지 반환
-def image_filter(input_img, method=0, k_size=1):
+def image_filter(input_img, method=0, k_size=3):
     return_img = input_img.copy()
 
     """
@@ -29,6 +45,20 @@ def image_filter(input_img, method=0, k_size=1):
      inverse filter 로 restore 가능하다면 수행
      
      
+     모든 전처리 과정을 한꺼번에 넣을 필요는 없음
+     쓸만한 전처리 과정만 가져오고, 나머지는 버리는게 더 좋음
+     앞서 말했듯 flag를 사용하여 어떤 전처리를 쓸건지 가져와야 할듯
+     -> 전역변수 bool flag 사용
+     
+     morphology true 의 경우 종류와 커널사이즈(3) 을 결정해야 함
+     어떻게 넣을지 생각 
+     1) 전역변수
+     2) 직접
+     
+     salt and pepper noise 가 완벽히 제거되지 않으면 linear HQ 효과없음
+     salt and pepper noise 에만 사용하는 HQ 만들기
+     
+     
      ################################################
      
      blur 처리
@@ -37,30 +67,46 @@ def image_filter(input_img, method=0, k_size=1):
     """
 
     # 처음 Histogram Equalization 내장함수 사용시 글자가 두꺼워지며 부정적인 영향을 줌
-    # return_img = cv2.equalizeHist(img)
+    # 선형 HQ 사용
+    if FIRST_LINEAR_HQ:
+        return_img = set_value_to_min_max(return_img)
 
     # gaussian noise 있을 경우 사용    /   잘못 사용시 글씨가 blur 처리 됨
     # noise_var = 100 일 때도 글자가 배경색이랑 비슷하면 blur 처리 됨
     # noise_var 을 줄여 글자가 blur 처리 안 되게 해야 함
     # gaussian noise를 사용자가 인지하고 입력하게끔 만들 예정
-    # return_img = adaptive_filtering(img, (7, 7), 50)
+    if ADAPTIVE:
+        return_img = adaptive_filtering(return_img, (7, 7), 50)
 
+    # salt and pepper noise 제거      median filter
+    # kernel size 5이상이면 글자 손상
+    if MEDIAN:
+        return_img = median_filter(return_img, 3)
+
+    # Homomorphic filter
     # 효과가 있음, c 값이 높고 high 값이 높으면 다 없어질 때가 있음
     # 전반적으로 검은글씨는 가늘어지고, 흰색글씨는 두꺼워짐
-    return_img = HF(return_img, cutoff=2, high=1.2, low=0.9, c=20)
+    if HOMOMORPHIC:
+        return_img = HF(return_img, cutoff=2, high=1.2, low=0.9, c=20)
 
     # HQ
-    return_img = set_value_to_min_max(return_img)
+    # salt and pepper noise가 완벽히 제거되지 않으면 효과없음  대안 필요
+    if SECOND_LINEAR_HQ:
+        return_img = set_value_to_min_max(return_img)
     # return_img = cv2.equalizeHist(return_img)     #linear하지 않음
 
     # Set binary image
-    # 128 기준으로 정확도가 떨어지는 문제 발생
-    return_img = get_binary_image(return_img)
+    # 128 기준으로 정확도가 떨어지는 문제 발생  /   조정
+    if BINARY:
+        return_img = get_binary_image(return_img)
 
     # Morphology
     # binary image 에서만 test 가능
     # image_filter 함수에서 method, k_size 입력이 없을시 수행되지 않음
-    return_img = morphology(return_img, method, k_size)
+    # 글자가 작으면 kernel size 3이어도 글자 손상 발생
+    # 글자가 적당히 크고 얇고 끊어질 때만 사용 가능할듯  /   binary 단계에서까지 글자가 손상됨
+    if MORPHOLOGY:
+        return_img = morphology(return_img, method, k_size)
 
     print("Print image")
     cv2.imshow('image', return_img)
@@ -70,7 +116,7 @@ def image_filter(input_img, method=0, k_size=1):
     return return_img
 
 
-# 가운데 패딩해주는 함수
+# 테두리 패딩해주는 함수
 def Padding(input_img, filter_size):
     print("Padding")
     iw, ih = input_img.shape
@@ -85,7 +131,7 @@ def Padding(input_img, filter_size):
 
 
 def adaptive_filtering(input_img, filter_size, noise_var):
-    print("Adaptive filter")
+    print("Adaptive filter...")
     iw, ih = input_img.shape
     fw, fh = filter_size
 
@@ -109,7 +155,7 @@ def adaptive_filtering(input_img, filter_size, noise_var):
 
 
 def HF(input_img, cutoff, low, high, c):  # Homomorphic filter
-    print("Homomorphic filter")
+    print("Homomorphic filter...")
     m, n = input_img.shape
 
     # Zero padding
@@ -196,9 +242,9 @@ def HF(input_img, cutoff, low, high, c):  # Homomorphic filter
     return idft2d
 
 
-def morphology(input_img, method, k_size):
+def morphology(input_img, method, k_size=3):
     """
-    :param input_img: input image
+    :param input_img: input_img image
     :param method: 1(erosion), 2(dilation), 3(opening), 4(closing)
     :param k_size: kernel size
     :return:
@@ -224,6 +270,17 @@ def morphology(input_img, method, k_size):
         morp_result = opening(input_img, k_size)
     elif method == 4:  # closing
         morp_result = closing(input_img, k_size)
+
+    if method == 1:
+        print("erosion...")
+    elif method == 2:
+        print("dilation...")
+    elif method == 3:
+        print("opening...")
+    elif method == 4:
+        print("closing...")
+    else:
+        print("No morphology filter...")
 
     return morp_result
 
@@ -257,7 +314,7 @@ def closing(input_img, k_size):
 
 
 def set_value_to_min_max(input_img):
-    print("HQ")
+    print("HQ...")
     return_img = input_img.copy()
     h, w = input_img.shape
     min_value = 0
@@ -279,7 +336,7 @@ def set_value_to_min_max(input_img):
 
 
 def get_binary_image(input_img):
-    print("get binary image")
+    print("Get binary image...")
     return_img = input_img.copy()
     h, w = input_img.shape
 
@@ -290,7 +347,35 @@ def get_binary_image(input_img):
             else:
                 return_img[i, j] = 255
 
+    # cv2.imshow('binary', return_img)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
+
     return return_img.astype(np.uint8)
+
+
+def median_filter(input_img, kernel_size):
+    print("median filter...")
+    return_img = input_img.copy()
+    m, n = input_img.shape
+    pad_size = kernel_size//2
+    padded_img = np.zeros((m + 2 * pad_size, n + 2 * pad_size))
+
+    for i in range(m):
+        for j in range(n):
+            padded_img[i + pad_size, j + pad_size] = input_img[i, j]
+
+    for i in range(m):  # 최대 m-1
+        for j in range(n):
+            partlist = np.zeros((kernel_size * kernel_size))
+            for x in range(kernel_size):    # 최대 kernel_size - 1
+                for y in range(kernel_size):
+                    partlist[x + y * kernel_size] = padded_img[i + x, j + y]    # 최대 m + kernel_size - 2
+            partlist.sort()
+            return_img[i, j] = partlist[(kernel_size * kernel_size) // 2]
+
+    return return_img
+
 
 if __name__ == "__main__":
 
@@ -305,10 +390,13 @@ if __name__ == "__main__":
     for file_name in file_list:
         img = cv2.imread(path_dir + '\\' + file_name, cv2.IMREAD_GRAYSCALE)
 
-        # Homomorphic filter 수행 금지
-        result_img = image_filter(img)
+        # img 변수 뒤에 다른 변수 없으면 morphology filter 수행 금지
+        # 필터링된 이미지를 result_img에 저장하고 cv2로 출력
+        result_img = image_filter(img, CLOSING)
+        # 이미지 저장
         cv2.imwrite(save_dir + '\\' + file_name + '_filtered.jpg', result_img)
 
+        # 필터링된 이미지에서 텍스트를 추출해서 output.txt에 작성
         result.write(pytesseract.image_to_string(save_dir + '\\' + file_name + '_filtered.jpg', lang='ENG',
                                                  config='--psm 4 -c preserve_interword_spaces=1') + '\n')
     result.close()
